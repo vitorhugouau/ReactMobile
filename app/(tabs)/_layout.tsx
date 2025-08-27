42fd6e194925738b9175da93f6eba1f7b6ca1910
Vitor Hugo <vitorhugkrodrigues@gmail.com>
2025-08-26 22:18:37 -0300

    feat: add post search by ID with comments using jsonplaceholder API

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,79 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useState } from 'react';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { StyleSheet, View } from 'react-native';
+import Tab from './../../components/Tab';
+import Calculator from './../../pages/ex1/Calculator';
+import StyleAndLayout from './../../pages/ex2/StyleAndLayout';
+import Props from './../../pages/ex3/Props';
+import FatherFunction from './../../pages/ex4/FatherFunction';
+import List from './../../pages/ex5/List';
+import Axios from './../../pages/ex6/Axios';
+import Buscar from './../../pages/ex7/Buscar';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const [tab, setTab] = useState('');
+
+  const getComponentByTab = () => {
+    switch (tab) {
+      case 'Calculadora':
+        return <Calculator />;
+      case 'Estilo e Layout':
+        return <StyleAndLayout />;
+      case 'Props':
+        return <Props />;
+      case 'Função Pai':
+        return <FatherFunction />;
+      case 'Lista':
+        return <List />;
+      case 'Axios':
+        return <Axios />
+      case 'Buscar':
+        return <Buscar/>
+      default:
+        return <Calculator />
+    }
+  };
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  <View style={styles.container}>
+    <View style={styles.contentContainer}>
+      {getComponentByTab()}
+    </View>
+
+    <View style={styles.tabBarContainer}>
+      <Tab setTab={setTab} />
+    </View>
+  </View>
+);
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f7', 
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 16,
+    paddingBottom: 80, 
+  },
+  tabBarContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 70,
+    backgroundColor: '#ffffff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+});
